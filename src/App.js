0709e0391b349973168b9e805f4fc9f9d1a36f88
ghassen kefi <ghassen.kefi.dev@gmail.com>
2021-06-29 14:21:55 +0200

    checking

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
+import React, { Component } from 'react';
+import Profile from './Components/Profile';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      Person:
+      {
+        fullname: "Monkey D.Luffy",
+        bio: "I Decided To Be Pirate King. I Don’t Care If I Die For It.",
+        profession: "Saving the world",
+        imgSrc: "https://cutewallpaper.org/21/luffy-profile-pic/Monkey-D-Luffy-YouTube.jpg"
+      },
+      shows: false,
+      counter:0
+    }
+  }
+
+  toggle = () => {
+    this.setState({ show: !this.state.show })
+  }
+
+ 
+  render() {
+    return (
+      <div className="App">
+        <button className="toggle-btn" onClick={this.toggle}>press</button><br></br>
+        {this.state.show && (
+          <Profile Person={this.state.Person} />
+        )}
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Components/Profile.js
@@ -0,0 +1,54 @@
+import React,{Component} from 'react'
+
+export default class Profile extends Component  {
+
+    _isMounted = false;
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            counter: 0
+        }
+    }
+
+    incrementCounter = () => {
+        const { counter } = this.state;
+        this.setState({ counter: counter + 1 });
+    };
+
+    componentDidMount() {
+        this._isMounted = true;
+        this._isMounted && setInterval(this.incrementCounter, 1000);
+    }
+
+    componentWillUnmount(){
+        this._isMounted = false;
+        clearInterval(this.state.counter)
+    }
+
+render(){
+    return (
+        <div className="Profile">
+            <div className="photo zoom">
+                <img className="photo" src={this.props.Person.imgSrc} alt="profile"></img>
+            </div>
+            <div className="fullname">
+                <span className="column">Full Name :</span>
+                <h2 className="column">{this.props.Person.fullname}</h2>
+            </div>
+            <div>
+                <span className="column">Profession :</span>
+                <h2 className="column">{this.props.Person.profession}</h2>
+            </div>
+            <div>
+                <span className="column">Bio :</span>
+                <h2 className="column">{this.props.Person.bio}</h2>
+            </div>
+
+            <div>
+                <h2 className="interval">the component has been mounted {this.state.counter} seconds ago</h2>
+            </div>
+        </div>
+    )
+}
+}
